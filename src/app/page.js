77b53bc88f6d68b57b8ee6ac7291969d8f9bca34
babulpro <sh8657706@gmail.com>
2025-01-20 00:49:44 +0600

    dda

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
- import React from 'react';
+ import MainNavBar from '@/components/Master/MainNavBar';
+import React from 'react';
+ async function getData() {
+  let category =(await(await fetch('http://localhost:3000/api/dashBoard/category')).json()).data 
+  return{
+      category 
+  }
+}
  
- const Page = () => {
+ const Page = async() => { 
+  let data =await getData()
   return (
     <div>
+      <MainNavBar data={data.category}/>
       <h1>Page</h1>
     </div>
   );
